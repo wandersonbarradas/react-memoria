3fd1101f61a19dfacbbf87b048ce7ae674267383
Wanderson Barradas <wandersonbarradas07@gmail.com>
2022-06-09 21:21:28 -0300

    a

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -35,7 +35,7 @@ export const GridArea = styled.div`
   flex: 1;
   display: flex;
   justify-content: flex-end;
-  @media (max-width: 750px) {
+  @media (max-width: 750px) { 
     justify-content: center;
     margin: 0 20px;
   }
